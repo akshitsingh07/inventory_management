98a494abceebce6ab78f30c61e5613eacf9b9d53
Akshit <akshitsingh1436@gmail.com>
2024-06-11 13:58:31 +0530

    New Build

--- inventory_management/inventory/views.py
+++ inventory_management/inventory/views.py
@@ -6,9 +6,13 @@ from .models import ExplicitPurchase, Purchase, Item, Category
 @cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def home(request):
     purchase = ExplicitPurchase.objects.all()
+    low_quantity_items = purchase.filter(quantity__lt=10)
+    low_quantity_count = low_quantity_items.count()
     print(purchase)
     context = {
-        "purchase": purchase
+        "purchase": purchase,
+        'low_quantity_items': low_quantity_items,
+        "low_quantity_count": low_quantity_count
     }
     print(context)
     return render(request, 'inventory/html/home.html', context)
